5226d5f4758f14f9c56a014d1d1c18b0915fa6a0
Eric Oliveira <oiericoi@hotmail.com>
2023-04-17 19:20:48 -0300

    build: start project

---
+++ src/main/java/com/api/myfavgames/MyFavGamesApplication.java
@@ -0,0 +1,13 @@
+package com.api.myfavgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFavGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFavGamesApplication.class, args);
+	}
+
+}
