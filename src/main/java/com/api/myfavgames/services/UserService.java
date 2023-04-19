a5679573c9e8f4b24bfb97cb7e7772b0cd3e957d
Eric Oliveira <oiericoi@hotmail.com>
2023-04-18 21:17:54 -0300

    feat: add findAll and findOne user methods

--- src/main/java/com/api/myfavgames/services/UserService.java
+++ src/main/java/com/api/myfavgames/services/UserService.java
@@ -2,9 +2,14 @@ package com.api.myfavgames.services;
 
 import com.api.myfavgames.models.UserModel;
 import com.api.myfavgames.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -14,10 +19,24 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public UserModel store(UserModel userModel) {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         var hashedPassword = encoder.encode(userModel.getPassword());
         userModel.setPassword(hashedPassword);
+
         return userRepository.save(userModel);
+    }
+
+    public boolean isEmailRegistered(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<UserModel> findOne(UUID id) {
+        return userRepository.findById(id);
     }
 }
