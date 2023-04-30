73f19c9ca7a691fe87505f715af5d26e0a3d7c0e
Eric Oliveira <oiericoi@hotmail.com>
2023-04-30 15:54:06 -0300

    refactor: modify index application

--- src/main/java/com/api/myfavgames/MyFavGamesApplication.java
+++ src/main/java/com/api/myfavgames/MyFavGamesApplication.java
@@ -2,10 +2,8 @@ package com.api.myfavgames;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MyFavGamesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyFavGamesApplication.class, args);
