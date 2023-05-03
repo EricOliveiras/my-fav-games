f632aa3ce7cad8b1ce2387dd6a3c7bde37b19784
Eric Oliveira <oiericoi@hotmail.com>
2023-05-03 08:21:15 -0300

    refactor: modify user service

--- src/main/java/com/api/myfavgames/modules/user/services/UserService.java
+++ src/main/java/com/api/myfavgames/modules/user/services/UserService.java
@@ -7,6 +7,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,94 +21,91 @@ import java.util.UUID;
 
 @Service
 public class UserService {
-    final UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+	@Transactional
+	public ResponseEntity<Object> store(UserDto userDto) {
+		boolean checkIfEmailRegistered = isEmailRegistered(userDto.getEmail());
 
-    @Transactional
-    public ResponseEntity<Object> store(UserDto userDto) {
-        boolean checkIfEmailRegistered = isEmailRegistered(userDto.getEmail());
+		if (checkIfEmailRegistered) {
+				return ResponseEntity.status(HttpStatus.CONFLICT).body("CONFLICT: Email already registered");
+		}
 
-        if (checkIfEmailRegistered) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("CONFLICT: Email already registered");
-        }
+		var hashedPassword = hashPassword(userDto.getPassword());
+		userDto.setPassword(hashedPassword);
 
-        var hashedPassword = hashPassword(userDto.getPassword());
-        userDto.setPassword(hashedPassword);
+		UserModel user = new UserModel();
 
-        UserModel user = new UserModel();
+		BeanUtils.copyProperties(userDto, user);
 
-        BeanUtils.copyProperties(userDto, user);
+		userRepository.save(user);
 
-        userRepository.save(user);
+		return ResponseEntity.ok().body("CREATED: User created");
+	}
 
-        return ResponseEntity.ok().body("CREATED: User created");
-    }
+	private boolean isEmailRegistered(String email) {
+		return userRepository.existsByEmail(email);
+	}
 
-    private boolean isEmailRegistered(String email) {
-        return userRepository.existsByEmail(email);
-    }
+	public ResponseEntity<List<UserModel>> findAll() {
+		return ResponseEntity.ok().body(userRepository.findAll());
+	}
 
-    public ResponseEntity<List<UserModel>> findAll() {
-        return ResponseEntity.ok().body(userRepository.findAll());
-    }
+	public ResponseEntity<Object> findOne(UUID id) {
+		Optional<UserModel> user = userRepository.findById(id);
 
-    public ResponseEntity<Object> findOne(UUID id) {
-        Optional<UserModel> user = userRepository.findById(id);
+		if (user.isEmpty()) {
+				return ResponseEntity.badRequest().body("NOT FOUND: User not found");
+		}
 
-        if (!user.isPresent()) {
-            return ResponseEntity.badRequest().body("NOT FOUND: User not found");
-        }
+		return ResponseEntity.ok().body(user);
+	}
 
-        return ResponseEntity.ok().body(user);
-    }
+	public ResponseEntity<Object> updateUser(UserDto userDto, UUID id) {
+		Optional<UserModel> user = userRepository.findById(id);
 
-    public ResponseEntity<Object> updateUser(UserDto userDto, UUID id) {
-        Optional<UserModel> user = userRepository.findById(id);
+		if (user.isEmpty()) {
+				return ResponseEntity.badRequest().body("NOT FOUND: User not found!");
+		}
 
-        if (!user.isPresent()) {
-            return ResponseEntity.badRequest().body("NOT FOUND: User not found!");
-        }
+		var userModel = user.get();
 
-        var userModel = user.get();
+		var checkPassrod = checkPassword(userModel.getPassword(), userDto.getPassword());
 
-        var checkPassrod = checkPassword(userModel.getPassword(), userDto.getPassword());
+		if (!checkPassrod) {
+				var newPassword = hashPassword(userDto.getPassword());
+				userModel.setPassword(newPassword);
+		}
 
-        if (!checkPassrod) {
-            var newPassword = hashPassword(userDto.getPassword());
-            userModel.setPassword(newPassword);
-        }
+		if (!Objects.equals(userDto.getEmail(), userModel.getEmail())) {
+				return ResponseEntity.badRequest().body("BAD REQUEST: Unable to update email");
+		}
 
-        if (!Objects.equals(userDto.getEmail(), userModel.getEmail())) {
-            return ResponseEntity.badRequest().body("BAD REQUEST: Unable to update email");
-        }
+		userModel.setUsername(userDto.getUsername());
 
-        userModel.setUsername(userDto.getUsername());
+		return ResponseEntity.ok().body(userRepository.save(userModel));
+	}
 
-        return ResponseEntity.ok().body(userRepository.save(userModel));
-    }
+	private String hashPassword(String password) {
+		return passwordEncoder.encode(password);
+	}
 
-    private String hashPassword(String password) {
-        return passwordEncoder.encode(password);
-    }
+	private boolean checkPassword(@Valid String bodyPassword, String password) {
+		return passwordEncoder.matches(password, bodyPassword);
+	}
 
-    private boolean checkPassword(@Valid String bodyPassword, String password) {
-        return passwordEncoder.matches(password, bodyPassword);
-    }
+	public ResponseEntity<Object> delete(UUID id) {
+		var user = userRepository.findById(id);
 
-    public ResponseEntity<Object> delete(UUID id) {
-        var user = userRepository.findById(id);
+		if (user.isEmpty()) {
+			return ResponseEntity.badRequest().body("NOT FOUND: User not found!");
+		}
 
-        if (user.isEmpty()) {
-            return ResponseEntity.badRequest().body("NOT FOUND: User not found!");
-        }
+		userRepository.delete(user.get());
 
-        userRepository.delete(user.get());
-
-        return ResponseEntity.ok().body("OK: Success in deleting user!");
-    }
+		return ResponseEntity.ok().body("OK: Success in deleting user!");
+	}
 }
