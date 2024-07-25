85104560b18e8db49a01d7ea06421edde45fcf27
Eric Oliveira <oiericoi@hotmail.com>
2024-07-25 09:51:29 -0300

    feat: add user response payload

---
+++ src/main/java/com/api/myfavgames/domain/user/payload/response/UserResponse.java
@@ -0,0 +1,60 @@
+package com.api.myfavgames.domain.user.payload.response;
+
+import java.sql.Timestamp;
+
+public class UserResponse {
+    private Long id;
+    private String fullName;
+    private String nickName;
+    private String email;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
