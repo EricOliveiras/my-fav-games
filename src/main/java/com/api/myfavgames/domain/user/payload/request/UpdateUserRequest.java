b5957d1f0fd9710772e7bfaf25dc9cfc5d2acbfa
Eric Oliveira <oiericoi@hotmail.com>
2024-07-25 10:10:09 -0300

    feat: add update user request payload

---
+++ src/main/java/com/api/myfavgames/domain/user/payload/request/UpdateUserRequest.java
@@ -0,0 +1,22 @@
+package com.api.myfavgames.domain.user.payload.request;
+
+public class UpdateUserRequest {
+    private String fullName;
+    private String nickName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+}
